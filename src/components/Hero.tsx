22d934abaff9ba5278b8b1458be8d6b0f79338fa
Quentin Le Sauce <quentin.lesauce@gmail.com>
2024-06-26 16:30:13 +0200

    custom Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,10 @@
-'use client';
-
 import React, { useState, useEffect } from 'react';
 import { MdDarkMode, MdLightMode } from 'react-icons/md';
 
 const Hero: React.FC = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const darkModeBackground = '/assets/cyberpunk_homeOffice.jpg';
+  const lightModeBackground = '/assets/zen_homeOffice.jpg';
 
   useEffect(() => {
     const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
@@ -34,7 +34,20 @@ const Hero: React.FC = () => {
   };
 
   return (
-    <section className="relative w-full h-[75vh] bg-gray-200 dark:bg-gray-800 text-black dark:text-white flex flex-col items-center justify-center">
+    <section
+      className="relative w-full h-[75vh] bg-gray-200 dark:bg-gray-800 text-black dark:text-white flex flex-col items-center justify-start"
+      style={{
+        backgroundImage: `url(${isDarkMode ? darkModeBackground : lightModeBackground})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <h1 className="text-4xl md:text-8xl font-bold mb-4 mt-12 text-shadow-xxl shadow-customShadowLight dark:shadow-customShadowDark">
+        Quentin Le Sauce
+      </h1>
+      <p className="text-xl md:text-2xl font-old mb-4 text-shadow-xl shadow-white dark:shadow-customShadowDark">
+        Web developer fullstack
+      </p>
       <div className="absolute top-4 right-4 z-10">
         <label className="flex items-center cursor-pointer">
           <div className="relative">
@@ -44,20 +57,14 @@ const Hero: React.FC = () => {
               checked={isDarkMode}
               onChange={toggleDarkMode}
             />
-            <div className="w-10 h-4 bg-gray-400 rounded-full shadow-inner dark:bg-gray-600"></div>
-            <div
-              className={`dot absolute w-6 h-6 bg-white rounded-full shadow -left-1 -top-1 transition ${
-                isDarkMode ? 'transform translate-x-full bg-blue-500' : ''
-              }`}
-            ></div>
+            <div className="w-10 h-4 bg-gray-400 rounded-full shadow-inner dark:bg-gray-600" />
+            <div className={`dot absolute w-6 h-6 bg-white rounded-full shadow -left-1 -top-1 transition ${isDarkMode ? 'transform translate-x-full bg-blue-500' : ''}`} />
           </div>
           <span className="ml-3 text-gray-700 dark:text-gray-300">
             {isDarkMode ? <MdDarkMode size={24} /> : <MdLightMode size={24} />}
           </span>
         </label>
       </div>
-      <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to My Portfolio</h1>
-      <p className="text-xl md:text-2xl mb-4">I&apos;m Quentin Le Sauce, a web developer.</p>
     </section>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   darkMode: 'class', // class-based dark mode
@@ -9,13 +10,32 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+        xl: '2px -2px 15px var(--tw-shadow-color)',
+        xxl: '2px 2px 32px var(--tw-shadow-color)',
       },
-    },
-  },
-  plugins: [],
+      colors: {
+        customShadowLight: '#ABA498', // Custom light text shadow color
+        customShadowDark: '#B7E7DB', // Custom dark text shadow color
+        lightBackground: '#DBDAD9', // Custom light background color
+        darkBackground: '#030303', // Custom dark background color
+      }
+		},
+	},
+	plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value: string) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      );
+    }),
+  ]
 };
 export default config;
